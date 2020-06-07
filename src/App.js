0576efbd501833d2dd86e9295d8b7b0c1d2d0189
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-06-06 21:56:36 -0700

    Create a Navbar and Tracks component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import './App.css';
+import Navbar from './components/layouts/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Index from './components/layouts/Index'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div class="container">
+            <Switch>
+              <Route exact path="/" component={Index}></Route>
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     )
   }
 }
